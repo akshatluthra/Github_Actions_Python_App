f1092f76aac2e8eeb8f980af961dafe5d4eec2a0
akshatluthra <akshatluthra981@gmail.com>
2026-03-11 00:22:34 +0530

    Correct changes in the files

--- test_app.py
+++ test_app.py
@@ -3,14 +3,18 @@ from app import app
 def test_add():
     assert app.add(5, 3) == 8
 
+
 def test_subtract():
     assert app.subtract(5, 3) == 2
+
 
 def test_multiply():
     assert app.multiply(5, 3) == 15
 
+    
 def test_divide():
     assert app.divide(5, 3) == 5 / 3
+
 
 def test_divide_by_zero():
     try:
