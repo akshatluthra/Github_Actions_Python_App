f161b4df4ef3c84f8577365b4122f5722750912d
akshatluthra <akshatluthra981@gmail.com>
2026-03-11 00:37:19 +0530

    New Change in test app

--- test_app.py
+++ test_app.py
@@ -2,23 +2,22 @@ from app import add, subtract, multiply, divide
 
 
 def test_add():
-    assert app.add(5, 3) == 8
+    assert add(5, 3) == 8
 
 
 def test_subtract():
-    assert app.subtract(5, 3) == 2
-
+    assert subtract(5, 3) == 2
 
 def test_multiply():
-    assert app.multiply(5, 3) == 15
+    assert multiply(5, 3) == 15
 
 
 def test_divide():
-    assert app.divide(5, 3) == 5 / 3
+    assert divide(5, 3) == 5 / 3
 
 
 def test_divide_by_zero():
     try:
-        app.divide(5, 0)
+        divide(5, 0)
     except ValueError as e:
         assert str(e) == "Cannot divide by zero"
