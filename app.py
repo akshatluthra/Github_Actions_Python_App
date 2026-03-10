1ab50a9f61bb649160febce7183702164fd48d0b
akshatluthra <akshatluthra981@gmail.com>
2026-03-11 00:01:48 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
+if __name__ == "__main__":
+    result = add(5, 3)
+    print(f"The result of adding 5 and 3 is: {result}")
+    result = subtract(5, 3)
+    print(f"The result of subtracting 3 from 5 is: {result}")
+    result = multiply(5, 3)
+    print(f"The result of multiplying 5 and 3 is: {result}")
+    result = divide(5, 3)
+    print(f"The result of dividing 5 by 3 is: {result}")
